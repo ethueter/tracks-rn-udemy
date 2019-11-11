d8823efbf891d2ba284bc08e1bfff1203a13eef9
Tyler Hueter <tylerhueter08@gmail.com>
2019-11-10 20:03:17 -0500

    navigation wiring

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { createAppContainer, creatSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/SignupScreen';
@@ -5,3 +9,20 @@ import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
 
+const switchNavigator = createSwitchNavigator({
+    loginFlow: createStackNavigator({
+      Signup: SignupScreen,
+      Signin: SigninScreen
+    }),
+    mainFlow: createBottomTabNavigator({
+      trackListFlow: createStackNavigator({
+        TrackList: TrackListScreen,
+        TrackDetail: TrackDetailScreen
+      }),
+      TrackCreate: TrackCreateScreen,
+      Account: AccountScreen
+    
+    })
+});
+
+export default createAppContainer(switchNavigator);
