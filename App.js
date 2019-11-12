9a856c9372fdac097add050c191fece71f96dc32
Tyler Hueter <tylerhueter08@gmail.com>
2019-11-12 16:23:34 -0500

    signup screen styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createAppContainer, creatSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AccountScreen from './src/screens/AccountScreen';
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,53 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, {useState} from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer'
 
 const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return (
-        <>
-            <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-            <Button
-                title="Go to Signin"
-                onPress={() => navigation.navigate('Signin')}
+        <View style={styles.container}>
+          <Spacer>
+           <Text h3>Sign Up for Tracker</Text>
+          </Spacer>
+          <Input 
+            label="Email"
+            value={email} 
+            onChangeText={setEmail}
+            autoCapitalize="none"
+            autoCorrect={false}
             />
-            <Button
-                title="Go to MainFlow"
-                onPress={() => navigation.navigate('mainFlow')}
+          <Spacer/>
+          <Input 
+            secureTextEntry
+            label="Password" 
+            value={password} 
+            onChangeText={setPassword}
+            autoCapitalize="none"
+            autoCorrect={false} 
             />
-        </>
+          <Spacer>
+           <Button title="Sign Up" />
+          </Spacer>
+        </View>
     );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    return {
+        header: null
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    }
+});
 
 export default SignupScreen;
