ace385993ff00021a879909ede51562e5691f17f
Tyler Hueter <tylerhueter08@gmail.com>
2019-11-18 19:16:32 -0500

    create NavLink/AuthForm components

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,41 +1,28 @@
-import React, {useState, useContext } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import Spacer from '../components/Spacer';
+import React, { useContext } from 'react';
+import { View, StyleSheet} from 'react-native';
 import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 
 const SignupScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    
 
 
     return (
         <View style={styles.container}>
-          <Spacer>
-           <Text h3>Sign Up for Tracker</Text>
-          </Spacer>
-          <Input 
-            label="Email"
-            value={email} 
-            onChangeText={setEmail}
-            autoCapitalize="none"
-            autoCorrect={false}
-            />
-          <Spacer/>
-          <Input 
-            secureTextEntry
-            label="Password" 
-            value={password} 
-            onChangeText={setPassword}
-            autoCapitalize="none"
-            autoCorrect={false} 
-            />
-          {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
-          <Spacer>
-           <Button title="Sign Up" onPress={() => signup({ email, password })} />
-          </Spacer>
+          <AuthForm 
+            headerText=""
+            errorMessage={state.errorMessage}
+            buttonText="Sign Up"
+            onSubmit={signup}
+          />
+          <NavLink 
+            routeName="Signin"
+            text="Already have an account? Sign in instead."
+          />
+          
         </View>
     );
 };
@@ -51,13 +38,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 200
-    },
-    errorMessage: {
-      fontSize: 16,
-      color: 'red',
-      marginLeft: 15,
-      marginTop: 15
     }
+    
 });
 
 export default SignupScreen;
