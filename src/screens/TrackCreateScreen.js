d3217ef4eb0ded21f82e9b0c45129baf0502036d
Tyler Hueter <tylerhueter08@gmail.com>
2019-11-30 17:58:13 -0500

    wire up location context

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,15 @@
 // import '../_mockLocation';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
-    const [err, setErr] = useState(null)
+    const { addLocation } = useContext(LocationContext);
+    const [err, setErr] = useState(null);
     
     const startWatching = async () => {
         try {
@@ -17,7 +19,7 @@ const TrackCreateScreen = () => {
                 timeInterval: 1000,
                 distanceInterval: 10
             }, (location) => {
-                console.log(location)
+                addLocation(location)
             })
         } catch (e) {
             setErr(e)
